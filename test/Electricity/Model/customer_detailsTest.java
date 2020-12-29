dc066c245111056967706cb81beeec80feda0ad6
AbdurRobTanvir <75908002+AbdurRobTanvir@users.noreply.github.com>
2020-12-30 01:48:15 +0600

    Add files via upload

---
+++ test/Electricity/Model/customer_detailsTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Electricity.Model;
+
+import Electricity.customer_details;
+import java.awt.event.ActionEvent;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tanvir
+ */
+public class customer_detailsTest {
+    
+     
+    public customer_detailsTest() {
+    }
+    
+     
+
+    /**
+     * Test of actionPerformed method, of class customer_details.
+     */
+    @Test
+    public void testActionPerformed() {
+         
+    }
+
+    /**
+     * Test of main method, of class customer_details.
+     */
+    @Test
+    public void testMain() {
+        
+    }
+    
+}
---
+++ test/Electricity/View/ProjectTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Electricity.View;
+
+import Electricity.Project;
+import java.awt.event.ActionEvent;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tanvir
+ */
+public class ProjectTest {
+    public ProjectTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of actionPerformed method, of class Project.
+     */
+    @Test
+    public void testActionPerformed() {
+         
+    }
+
+    /**
+     * Test of main method, of class Project.
+     */
+    @Test
+    public void testMain() {
+         
+    }
+    
+}
